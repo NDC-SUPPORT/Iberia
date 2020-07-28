9afdd6b34ba7fa676707ba994ae0d152f3509feb
YERROS <aromanillos@viewnext.com>
2020-07-28 10:57:20 +0200

    Mails TimeOut

---
+++ src/scripts/Mails_TimeOut.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+fch=$1
+hIni=$2
+hFin=$3
+curl -XGET 'http://ibisesdata.corp.iberia.es:9200/logstash-business-ndc-checkout-debug-*/sse-orm.mail-manage.native-mail-provider-send.1.debug/_search?size=2&from=0' -u support:HtaCFjwHTZGa98Rf -d '{
+
+  "sort": [
+    {
+      "@timestamp": {
+        "order": "asc"
+      }
+    }
+  ],
+  "query": {
+    "bool": {
+      "must": [
+      	{
+          "query_string": {
+            "query": "kpi.parameters.mailProviderRequest.body.salesforceRequest.salesforceBodyRequest.json_data.string:(*locator*)",
+            "analyze_wildcard": true
+          }
+        },
+        {
+          "match_phrase": {
+            "client": {
+              "query": "ndc"
+            }
+          }
+        },
+        {
+          "range": {
+            "elapsedTime": {
+              "gte": 60000,
+              "lt": null
+            }
+          }
+        },
+		{
+			"range": {
+				"@timestamp": {
+					"from": "'${fch}'T'${hIni}'.000",
+					"to":   "'${fch}'T'${hFin}'.999",
+					"time_zone": "Europe/Madrid",
+					"include_lower": true,
+					"include_upper": true
+				}
+			}
+		}
+      ]
+    }
+  },
+  "_source": ["request","@timestamp","kpi.parameters.mailProviderRequest.body.salesforceRequest","user"]
+  
+}'
